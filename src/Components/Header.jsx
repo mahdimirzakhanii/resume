aac787d5e1642be893b054184964dea28c89dfea
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-10-03 00:04:23 +0330

    edit header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,4 +1,4 @@
-import myImg from "../../public/assets/img/prof1.jpg";
+import myImg from "../../public/assets/img/prof2 (3).jpg";
 import { Typewriter } from "react-simple-typewriter";
 import { Link } from "react-scroll";
 import AOS from "aos";
@@ -8,38 +8,36 @@ function Header() {
   AOS.init();
   return (
     <div className="info w-full mt-44 z-20 lg:h-full flex flex-col lg:flex-row justify-between items-center ">
-      {/* <div style={{ width: '100%', height: '600px', position: 'relative' }}> */}
-        <div
-          className="name w-full md:w-1/2 flex items-start flex-col overflow-hidden"
-          data-aos="fade-right"
-          data-aos-offset="100"
-          data-aos-easing="ease-in-sine"
-          data-aos-duration="1000"
-        >
+      <div
+        className="name w-full gap-3 md:gap-0 md:w-1/2 flex items-start flex-col overflow-hidden"
+        data-aos="fade-right"
+        data-aos-offset="100"
+        data-aos-easing="ease-in-sine"
+        data-aos-duration="1000"
+      >
+        <div className="hello flex gap-1 md:gap-2 items-end w-full">
+          <h1 className="dark:text-primary text-tertiary text-5xl lg:text-6xl ">
+            Hello
+          </h1>
+          <span className="bottom-1 left-[104%] xl:left-[9.5vw] mb-1 w-2 h-2 md:h-3 md:w-3 bg-secondary rounded-full"></span>
+          <h4 className="dark:text-primary text-tertiary text-3xl lg:text-4xl">
+            I&apos;am
+          </h4>
+        </div>
 
-          <div className="hello flex gap-2 items-end ">
-            <h1 className="dark:text-primary text-tertiary text-5xl lg:text-6xl ">
-              Hello
-            </h1>
-            <span className="bottom-1 left-[104%] xl:left-[9.5vw] mt-1 h-3 w-3 bg-secondary rounded-full"></span>
-            <h4 className="dark:text-primary text-tertiary text-3xl lg:text-4xl">
-              I&apos;am
-            </h4>
-          </div>
-
-          <div className="flex items-end md:-ml-2 justify-center">
-            <p className=" text-secondary leading-0 font-semibold text-8xl lg:text-[200px]">
-              M
+        <div className="flex items-end justify-start w-full -ml-2 ">
+          <p className=" text-secondary leading-0 font-semibold text-8xl lg:text-[200px]">
+            M
+          </p>
+          <div className="flex flex-col items-start mb-1 md:mb-4 md:gap-2">
+            <p className="dark:text-primary text-tertiary text-3xl lg:text-6xl">
+              AHDI
             </p>
-            <div className="flex flex-col items-start mb-4 gap-2">
-              <p className="dark:text-primary text-tertiary text-4xl lg:text-6xl">
-                AHDI
-              </p>
-              <p className="dark:text-primary text-tertiary text-4xl lg:text-6xl">
-                IRZAKHANI
-              </p>
-            </div>
+            <p className="dark:text-primary text-tertiary text-3xl lg:text-6xl">
+              IRZAKHANI
+            </p>
           </div>
+        </div>
         {/* </div> */}
         <div className="developer mt-0 lg:mt-6">
           <h3 className="dark:text-primary text-tertiary text-3xl md:text-4xl ">
@@ -73,15 +71,15 @@ function Header() {
         data-aos-duration="2000"
       >
         <div className="border-img absolute bg-gradient-to-b from-transparent to-secondary
-          w-full md:w-[300px] h-[400px] left-5 top-5 rounded-br-[100px]"></div>
+          w-full md:w-[300px] h-[400px] left-4 top-4 rounded-br-[100px]"></div>
         <img
           src={myImg}
-          className="w-full md:max-w-[300px] h-[400px] relative rounded-br-[100px]"
+          className="h-[400px] relative rounded-br-[100px]"
           alt=""
         />
       </div>
 
-    </div>
+    </div >
   );
 }
 
