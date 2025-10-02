01701eadd290256c7f71e9f61431bd51a4095efb
mahdimirzakhani4 <mahdimirzakhani85@gmail.com>
2025-10-03 00:49:23 +0330

    fixed bug in text header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -7,15 +7,15 @@ import "aos/dist/aos.css";
 function Header() {
   AOS.init();
   return (
-    <div className="info w-full mt-44 z-20 lg:h-full flex flex-col lg:flex-row justify-between items-center ">
+    <div className=" w-full mt-44 z-20 lg:h-full flex flex-col lg:flex-row justify-between items-center ">
       <div
-        className="name w-full gap-3 md:gap-0 md:w-1/2 flex items-start flex-col overflow-hidden"
+        className="w-full lg:w-1/2 gap-3 lg:gap-0 flex items-start flex-col overflow-hidden"
         data-aos="fade-right"
         data-aos-offset="100"
         data-aos-easing="ease-in-sine"
         data-aos-duration="1000"
       >
-        <div className="hello flex gap-1 md:gap-2 items-end w-full">
+        <div className=" flex gap-1 md:gap-2 justify-start items-end w-full">
           <h1 className="dark:text-primary text-tertiary text-5xl lg:text-6xl ">
             Hello
           </h1>
@@ -26,32 +26,30 @@ function Header() {
         </div>
 
         <div className="flex items-end justify-start w-full -ml-2 ">
-          <p className=" text-secondary leading-0 font-semibold text-8xl lg:text-[200px]">
+          <p className=" text-secondary leading-[120px] lg:leading-[200px] font-semibold text-[150px] lg:text-[200px]">
             M
           </p>
-          <div className="flex flex-col items-start mb-1 md:mb-4 md:gap-2">
-            <p className="dark:text-primary text-tertiary text-3xl lg:text-6xl">
+          <div className="flex flex-col gap-2 items-start -mb-1 lg:mb-4 lg:gap-2">
+            <p className="dark:text-primary leading-none text-tertiary text-[40px] lg:text-6xl">
               AHDI
             </p>
-            <p className="dark:text-primary text-tertiary text-3xl lg:text-6xl">
+            <p className="dark:text-primary leading-none text-tertiary text-[40px] lg:text-6xl">
               IRZAKHANI
             </p>
           </div>
         </div>
         {/* </div> */}
-        <div className="developer mt-0 lg:mt-6">
-          <h3 className="dark:text-primary text-tertiary text-3xl md:text-4xl ">
-            <Typewriter
-              words={["Front-End Developer", "React Developer"]}
-              loop={5}
-              cursor
-              cursorStyle="|"
-              typeSpeed={150}
-              deleteSpeed={150}
-              delaySpeed={3000}
-            />
-          </h3>
-        </div>
+        <h3 className="dark:text-primary text-tertiary text-4xl ">
+          <Typewriter
+            words={["Front-End Developer", "React Developer"]}
+            loop={5}
+            cursor
+            cursorStyle="|"
+            typeSpeed={150}
+            deleteSpeed={150}
+            delaySpeed={3000}
+          />
+        </h3>
         <div className="btn-cv relative py-[7px] lg:left-20 mt-10 mx-auto lg:mx-0">
           <Link
             to="about"
